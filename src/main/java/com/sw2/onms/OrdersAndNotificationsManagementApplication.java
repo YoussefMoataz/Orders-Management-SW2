d0898f3357146d6c1aee2d37b76a59b3b3c2c0e0
Youssef Moataz <yousefcrunshy03@gmail.com>
2023-12-24 13:52:15 +0200

    Initial commit.

---
+++ src/main/java/com/sw2/onms/OrdersAndNotificationsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sw2.onms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersAndNotificationsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersAndNotificationsManagementApplication.class, args);
+	}
+
+}

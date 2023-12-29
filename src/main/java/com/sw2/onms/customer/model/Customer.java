eb473feac9116939e3e36ee5fead0b3c4d71723a
Ibn-Maged <mohamedmaged1607@gmail.com>
2023-12-29 20:56:22 +0200

    added mobile number and preferred language and preferred notification channel to customer model

--- src/main/java/com/sw2/onms/customer/model/Customer.java
+++ src/main/java/com/sw2/onms/customer/model/Customer.java
@@ -1,16 +1,69 @@
 package com.sw2.onms.customer.model;
 
+import com.sw2.onms.NotificationManagement.NotificationSenderType;
+import com.sw2.onms.NotificationManagement.TemplateCreation.Language;
+
 public class Customer {
     private String name;
     private String email;
     private String password;
     private Double balance;
+    private String mobileNumber;
+    private Language preferredLanguage;
+    private NotificationSenderType preferredNotificationChannel;
 
-    public Customer(String name, String email, String password, Double balance) {
+    public Customer(String name, String email, String password, Double balance, String mobileNumber) {
         this.name = name;
         this.email = email;
         this.password = password;
         this.balance = balance;
+        this.mobileNumber = mobileNumber;
+        this.preferredLanguage = Language.English;
+        this.preferredNotificationChannel = NotificationSenderType.Email;
+    }
+
+    public Customer(String name,
+                    String email,
+                    String password,
+                    Double balance,
+                    String mobileNumber,
+                    Language preferredLanguage,
+                    NotificationSenderType preferredNotificationChannel) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.balance = balance;
+        this.mobileNumber = mobileNumber;
+        this.preferredLanguage = preferredLanguage;
+        this.preferredNotificationChannel = preferredNotificationChannel;
+    }
+    public Customer(String name,
+                    String email,
+                    String password,
+                    Double balance,
+                    String mobileNumber,
+                    Language preferredLanguage) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.balance = balance;
+        this.mobileNumber = mobileNumber;
+        this.preferredLanguage = preferredLanguage;
+        this.preferredNotificationChannel = NotificationSenderType.Email;
+    }
+    public Customer(String name,
+                    String email,
+                    String password,
+                    Double balance,
+                    String mobileNumber,
+                    NotificationSenderType preferredNotificationChannel) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.balance = balance;
+        this.mobileNumber = mobileNumber;
+        this.preferredLanguage = Language.English;
+        this.preferredNotificationChannel = preferredNotificationChannel;
     }
 
     public String getName() {
@@ -43,5 +96,29 @@ public class Customer {
 
     public void setBalance(Double balance) {
         this.balance = balance;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public Language getPreferredLanguage() {
+        return preferredLanguage;
+    }
+
+    public void setPreferredLanguage(Language preferredLanguage) {
+        this.preferredLanguage = preferredLanguage;
+    }
+
+    public NotificationSenderType getPreferredNotificationChannel() {
+        return preferredNotificationChannel;
+    }
+
+    public void setPreferredNotificationChannel(NotificationSenderType preferredNotificationChannel) {
+        this.preferredNotificationChannel = preferredNotificationChannel;
     }
 }
